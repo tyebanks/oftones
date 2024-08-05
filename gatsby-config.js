34cdc5ff50a28d9bca636892bccf6db101f28c37
tyebanks <ebankst07@gmail.com>
2024-08-05 22:56:54 +0100

    updated gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
         {
             resolve: `gatsby-source-wordpress`,
             options: {
-                url: `https://voiceoftones.com/graphql`,
+                url: `https://cms.voiceoftones.com/graphql`,
             },
         },
     ],
